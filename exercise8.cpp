075e06fecb0b82ffa924afdf790d12e15ecae6c0
LordLukin <lukin1000@gmail.com>
2018-08-05 11:13:36 +0200

    Update exercise8.cpp

--- exercise8.cpp
+++ exercise8.cpp
@@ -14,9 +14,9 @@ int main()
 {
     std::array<double, 6> a{5.0, 4.0, -1.4, 7.9, -8.22, 0.4};
     std::sort(a.begin(), a.end(), AbsoluteCompare());
-    std::sort(a.begin(), a.end());/*, [](double a, double b){
+    std::sort(a.begin(), a.end(), [](double a, double b){
         return std::abs(a) < std::abs(b);
-    });*/
+    });
     for (auto & el : a)
     {
         std::cout << el << " ";
